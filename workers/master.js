003eae0626ca1760933cea33311e5b5758e9836d
grymyk <mykola.grytsai>
2016-10-18 17:29:39 +0300

    refactor spaces

--- workers/master.js
+++ workers/master.js
@@ -1,39 +1,39 @@
 'use strict';
 
-module.exports = function() {
+module.exports = () => {
+	let cpuCount = api.os.cpus().length;
+	let workers = [];
 
-  let cpuCount = api.os.cpus().length;
+	for (let i = 0; i < cpuCount; i++) {
+		let worker = api.cluster.fork();
 
-  let workers = [];
-  for (let i = 0; i < cpuCount; i++) {
-    let worker = api.cluster.fork();
-    workers.push(worker);
-  }
+		workers.push(worker);
+	}
 
-  let task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
-  let results = [];
+	let task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
+	let results = [];
 
-  workers.forEach(function(worker) {
+	workers.forEach( (worker) => {
+		worker.send({ task: task });
 
-    worker.send({ task: task });
+		worker.on('exit', (code) => {
+			console.log('exit ' + worker.process.pid + ' ' + code);
 
-    worker.on('exit', function (code) {
-      console.log('exit ' + worker.process.pid + ' ' + code);
-    });
+		});
 
-    worker.on('message', function (message) {
-      console.log(
-        'message from worker ' + worker.process.pid + ': ' +
-        JSON.stringify(message)
-      );
-      results.push(message.result);
+		worker.on('message', (message) => {
+			console.log(
+				'message from worker ' +
+				worker.process.pid + ': ' +
+				JSON.stringify(message)
+			);
 
-      if (results.length === cpuCount) {
-        process.exit(1);
-      }
+			results.push(message.result);
 
-    });
-
-  });
-
+			if (results.length === cpuCount) {
+				process.exit(1);
+			}
+		});
+	});
 };
+
--- workers/worker.js
+++ workers/worker.js
@@ -1,16 +1,21 @@
 'use strict';
 
 module.exports = () => {
-  console.log('Hello from worker ' + process.pid + ' ' + api.cluster.worker.id);
+	console.log(
+		'Hello from worker ' +
+		process.pid + ' ' +
+		api.cluster.worker.id
+	);
 
-  process.on('message', (message) => {
-    console.log(
-      'message to worker ' + process.pid +
-      ' from master: ' + JSON.stringify(message)
-    );
-    process.send({
-      result: message.task.map((item) => item * 2)
-    });
-  });
+	process.on('message', (message) => {
+		console.log(
+			'message to worker ' + process.pid +
+			' from master: ' + JSON.stringify(message)
+		);
 
+		process.send({
+			result: message.task.map((item) => item * 2)
+		});
+	});
 };
+
