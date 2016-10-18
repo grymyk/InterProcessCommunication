68f65e92334a0f5b4b11b5e7945a4f1202f399bb
grymyk <mykola.grytsai>
2016-10-18 20:50:23 +0300

    2 main task

--- workers/master.js
+++ workers/master.js
@@ -1,24 +1,28 @@
 'use strict';
 
+let getSubTasks = require('./subTasks.js');
+
 module.exports = () => {
 	let cpuCount = api.os.cpus().length;
 	let workers = [];
 
-	for (let i = 0; i < cpuCount; i++) {
+	for (let i = 0; i < cpuCount; i += 1) {
 		let worker = api.cluster.fork();
 
 		workers.push(worker);
 	}
 
 	let task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
+
+	let subTask = getSubTasks(cpuCount, task);
+
 	let results = [];
 
-	workers.forEach( (worker) => {
-		worker.send({ task: task });
+	workers.forEach( (worker, i) => {
+		worker.send({ task: subTask[i] });
 
 		worker.on('exit', (code) => {
 			console.log('exit ' + worker.process.pid + ' ' + code);
-
 		});
 
 		worker.on('message', (message) => {
