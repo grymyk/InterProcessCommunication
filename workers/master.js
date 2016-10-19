bdd07506e2701adb873b131e05fb0e37f0b95a4b
grymyk <grymyk@gmail.com>
2016-10-19 12:35:01 +0300

    refactor space

--- workers/master.js
+++ workers/master.js
@@ -1,10 +1,10 @@
 'use strict';
 
-module.exports = function() {
-
+module.exports = () => {
   let cpuCount = api.os.cpus().length;
 
   let workers = [];
+
   for (let i = 0; i < cpuCount; i++) {
     let worker = api.cluster.fork();
     workers.push(worker);
@@ -13,27 +13,25 @@ module.exports = function() {
   let task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
   let results = [];
 
-  workers.forEach(function(worker) {
-
+  workers.forEach( (worker) => {
     worker.send({ task: task });
 
-    worker.on('exit', function (code) {
+    worker.on('exit', (code) => {
       console.log('exit ' + worker.process.pid + ' ' + code);
     });
 
-    worker.on('message', function (message) {
+    worker.on('message', (message) => {
       console.log(
         'message from worker ' + worker.process.pid + ': ' +
         JSON.stringify(message)
       );
+
       results.push(message.result);
 
       if (results.length === cpuCount) {
         process.exit(1);
       }
-
     });
-
   });
-
 };
+
--- workers/multicore.js
+++ workers/multicore.js
@@ -1,12 +1,18 @@
 'use strict';
 
 global.api = {};
+
 api.cluster = require('cluster');
 api.os = require('os');
 
 global.application = {};
+
 application.master = require('./master.js');
 application.worker = require('./worker.js');
 
-if (api.cluster.isMaster) application.master();
-else application.worker();
+if (api.cluster.isMaster) {
+    application.master();
+} else {
+    application.worker();
+}
+
